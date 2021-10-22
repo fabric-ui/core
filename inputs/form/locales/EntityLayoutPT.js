bcd0dda1c54813b8ee8f9aa65ece7b58d646eca2
facobackup <gustavomicaelbarbosa@gmail.com>
2021-10-22 19:11:33 -0300

    Commit inicial

---
+++ inputs/form/locales/EntityLayoutPT.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default {
+    history: 'Histórico de mudanças',
+    edit: 'Editar',
+    overview: 'Visão geral',
+    info: 'Informações',
+    create: 'Criar',
+    save: 'Salvar'
+}
