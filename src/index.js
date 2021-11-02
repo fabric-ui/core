28ec554d092e744ba8dc3bad8d96b4bd9ac140af
facobackup <gustavomicaelbarbosa@gmail.com>
2021-11-02 14:44:18 -0300

    Fix index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Tabs from './core/navigation/tabs/Tabs'
 import VerticalTabs from './core/navigation/tabs/VerticalTabs'
-import List from './core/list/List'
-import Modal from './core/misc/modal/Modal'
+import List from './core/visualization/list/List'
+import Modal from './core/navigation/modal/Modal'
 import Selector from './core/inputs/selector/Selector'
 import Form from './core/inputs/form/Form'
 import FormRow from './core/inputs/form/FormRow'
@@ -10,14 +10,14 @@ import DateField from './core/inputs/date/DateField'
 import DropDownField from './core/inputs/dropdown/DropDownField'
 import FileField from './core/inputs/file/FileField'
 import MultiSelectField from './core/inputs/multiselect/MultiSelectField'
-import Navigation from './core/navigation/layout/Navigation'
-import Requester from './core/misc/requester/Requester'
+import Layout from './core/navigation/layout/Layout'
+import Requester from './core/feedback/requester/Requester'
 import TextField from './core/inputs/text/TextField'
-import ToolTip from './core/misc/tooltip/ToolTip'
-import useQuery from './core/shared/hooks/useQuery'
+import ToolTip from './core/feedback/tooltip/ToolTip'
+import useQuery from './core/visualization/hooks/useQuery'
 
-import ThemeContext from './core/theme/ThemeContext'
-import ThemeProvider from './core/theme/ThemeProvider'
+import ThemeContext from './core/misc/theme/ThemeContext'
+import ThemeProvider from './core/misc/theme/ThemeProvider'
 
 export {
   useQuery,
@@ -30,7 +30,9 @@ export {
   DropDownField,
   FileField,
   MultiSelectField,
-  Navigation, ThemeContext, ThemeProvider,
+  Layout,
+  ThemeContext,
+  ThemeProvider,
   Requester,
   TextField,
   ToolTip
