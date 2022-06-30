fa31cb90663b2f28bb74ec8008651a2488b0e7f8
facobackup <gustavomicaelbarbosa@gmail.com>
2022-06-30 12:50:21 -0300

    Ajustes

--- src/navigation/dropdown/Dropdown.js
+++ src/navigation/dropdown/Dropdown.js
@@ -2,134 +2,95 @@ import React, {useCallback, useEffect, useMemo, useRef, useState} from "react"
 import PropTypes from "prop-types"
 import styles from "./styles/Dropdown.module.css"
 import Button from "../../inputs/button/Button"
-import Modal from "../modal/Modal"
 import DropdownOptions from "./DropdownOptions"
 import DropdownProvider from "./DropdownProvider"
 import Icon from "../../visualization/icon/Icon"
+import Content from "./Content";
 
 export default function Dropdown(props) {
-    const [open, setOpen] = useState(false)
+   const [open, setOpen] = useState(false)
+
+   const ref = useRef()
 
 
-    const modalRef = useCallback((node) => {
-        if(node) {
-            const bBox = node.getBoundingClientRect()
-            const button = ref.current?.getBoundingClientRect()
-            const body = document.body.getBoundingClientRect()
+   useEffect(() => {
+      const resizeOBS = new ResizeObserver(() => {
+         setOpen(false)
+      })
+      resizeOBS.observe(document.body)
+      return () => resizeOBS.disconnect()
+   }, [])
 
-            const offX = 8,
-                offY = 4
-            let y = `calc(50% + ${button.height / 2 }px)`,
-                x = 0
+   const children = React.Children.toArray(props.children)
+   return (
+      <>
+         <Button
+            highlight={open || props.highlight}
+            attributes={{
+               ...props.attributes,
+               "data-open": `${open}`
+            }}
+            reference={ref}
+            styles={{
+               ...props.styles,
+               display: props.hideArrow ? undefined : "flex",
+               alignItems: props.hideArrow ? undefined : "center",
+               gap: props.hideArrow ? undefined : "4px"
 
+            }}
+            variant={props.variant}
+            onClick={() => {
+               if (props.onOpen)
+                  props.onOpen()
+               setOpen(true)
+            }}
+            disabled={props.disabled}
+            className={props.className}>
+            {children.filter(e => e?.type?.name !== DropdownOptions.name).map(l => l)}
+            {props.hideArrow ? null : <Icon>arrow_drop_down</Icon>}
+         </Button>
+         {open && typeof window !== "undefined" ?
+            <Content
+               {...props}
+               parent={ref.current}
+               close={() => {
+                  if (props.onClose)
+                     props.onClose()
 
-            if (bBox.y < 0)
-                y = "calc(50% + " + ((-bBox.y / 2 - offY + button.height / 2) / 2) + "px)"
-
-            if (bBox.x < 0)
-                x = (-bBox.x + offX) + "px"
-
-            if ((button.y + bBox.height + button.height) > body.height)
-                y = `calc(-50% - ${button.height/2}px)`
-
-
-            if ((bBox.x + bBox.width) > body.width)
-                x = (body.width - (bBox.x + bBox.width) - offX) + "px"
-
-            node.parentNode.style.transform = `translate(${x}, ${y})`
-        }
-    }, [])
-    const ref = useRef()
-
-    useEffect(() => {
-        const resizeOBS = new ResizeObserver(() => {
-            setOpen(false)
-        })
-        resizeOBS.observe(document.body)
-        return () => resizeOBS.disconnect()
-    }, [])
-
-    const content = useMemo(() => {
-        return React.Children.toArray(props.children).find(e => {
-            return e?.type?.name === DropdownOptions.name
-        })
-    }, [props.children])
-
-    const label = useMemo(() => {
-        return React.Children.toArray(props.children).filter(e => !e?.type?.name || e?.type.name !== DropdownOptions.name)
-    }, [props.children])
-    console.log(open)
-    return (
-        <span>
-            <Button
-
-                highlight={open || props.highlight}
-                attributes={{
-                    ...props.attributes,
-                    "data-open": `${open}`
-                }}
-                reference={ref}
-                styles={{
-                    ...props.styles,
-                    display: props.hideArrow ? undefined : "flex",
-                    alignItems: props.hideArrow ? undefined : "center",
-                    gap: props.hideArrow ? undefined : "4px"
-
-                }}
-                variant={props.variant}
-                onClick={() => {
-                    if (props.onOpen)
-                        props.onOpen()
-                    setOpen(true)
-                }}
-                disabled={props.disabled}
-                className={props.className}>
-                {label.map(l => l)}
-
-                {props.hideArrow ? null : <Icon>arrow_drop_down</Icon>}
-
-
-            </Button>
-            <Modal
-                variant={"fit"}
-                styles={{transition: props.animate ? undefined : "none"}}
-                blurIntensity={0}
-                className={[styles.buttons, props.wrapperClassname].join(" ")}
-                animationStyle={props.animate ? "fade" : "none"}
-                open={open}
-                handleClose={() => {
-                    setOpen(false)
-                    if (props.onClose)
-                        props.onClose()
-                }}>
-                <div ref={modalRef} className={props.modalContentClassname} style={props.modalContentStyles}>
-                    <DropdownProvider.Provider value={{
+                  setOpen(false)
+               }}>
+               {open ? (
+                  <DropdownProvider.Provider
+                     value={{
                         setOpen,
                         open
-                    }}>
-                        {content}
-                    </DropdownProvider.Provider>
-                </div>
-            </Modal>
-        </span>
-    )
+                     }}
+                  >
+
+                     {children.find(e => e?.type?.name === DropdownOptions.name)}
+                  </DropdownProvider.Provider>
+               ) : null}
+            </Content>
+            :
+            null
+         }
+      </>
+   )
 }
 
 Dropdown.propTypes = {
-    modalContentClassname: PropTypes.string,
-    modalContentStyles: PropTypes.object,
+   modalClassName: PropTypes.string,
+   modalStyles: PropTypes.object,
 
-    animate: PropTypes.bool,
-    onOpen: PropTypes.func,
-    onClose: PropTypes.func,
+   onOpen: PropTypes.func,
+   onClose: PropTypes.func,
 
-    attributes: PropTypes.object,
-    hideArrow: PropTypes.bool,
-    wrapperClassname: PropTypes.string,
-    highlight: PropTypes.bool,
-    variant: PropTypes.oneOf(["minimal", "filled", "outlined", "minimal-horizontal"]),
-    className: PropTypes.string,
-    styles: PropTypes.object,
-    disabled: PropTypes.bool,
-    children: PropTypes.node,
+   attributes: PropTypes.object,
+   hideArrow: PropTypes.bool,
+   highlight: PropTypes.bool,
+   variant: PropTypes.oneOf(["minimal", "filled", "outlined", "minimal-horizontal"]),
+   className: PropTypes.string,
+   styles: PropTypes.object,
+   disabled: PropTypes.bool,
+   children: PropTypes.node,
 }
--- src/navigation/modal/Modal.js
+++ src/navigation/modal/Modal.js
@@ -4,90 +4,56 @@ import styles from "./styles/Modal.module.css"
 import ThemeContext from "../../misc/hooks/ThemeContext"
 import useModal from "./hooks/useModal"
 import * as DOM from "react-dom/client"
+import * as ReactDOM from "react-dom";
 
 export default function Modal(props) {
+   const {
+      animations,
+      target,
+   } = useModal(props)
+   const context = useContext(ThemeContext)
+   const handleMouseDown = (event) => {
+      if (!document.elementsFromPoint(event.clientX, event.clientY).includes(target.current))
+         props.handleClose()
+   }
+   const [isClosed, setIsClosed] = useState(!props.open)
+   useEffect(() => {
+      document.addEventListener("mousedown", handleMouseDown)
+      return () => document.removeEventListener("mousedown", handleMouseDown)
+   }, [props.open])
 
-    const {
-        animations, mountingPoint, animate, target, source, getParentPosition
-    } = useModal({
-        animationStyle: props.animationStyle,
-        variant: props.variant
-    })
-    const context = useContext(ThemeContext)
-    const handleMouseDown = (event) => {
-        if (!document.elementsFromPoint(event.clientX, event.clientY).includes(target.current) && alreadyRendered)
-            props.handleClose()
-    }
-    const [alreadyRendered, setAlreadyRendered] = useState(false)
-    const localRoot = useRef()
-    const rooted = useRef(false)
-    useEffect(() => {
-        const  renderTarget = document.createElement("div")
-        document.body.appendChild(renderTarget)
-        renderTarget.style.position = "fixed"
-        renderTarget.style.zIndex = "999"
-        mountingPoint.current = renderTarget
-
-        return () => document.body.removeChild(renderTarget)
-    }, [])
-
-    useEffect(() => {
-        if (props.open) {
-            const position = getParentPosition()
-            if(!localRoot.current || !rooted.current) {
-                rooted.current = true
-                localRoot.current = DOM.createRoot(mountingPoint.current)
-            }
-            localRoot.current.render((
-                <div
-                    style={{
-                        ...props.variant === "fit" ? {} : {backdropFilter: `blur(${props.blurIntensity ? props.blurIntensity : "10px"})`},
-                        ...position,
-                        ...context.themes
-                    }}
-                    className={[styles.wrapper, props.variant === "fit" ? styles.fitContent : styles.fitPage, props.wrapperClassName, context.dark ? context.styles.dark : context.styles.light].join(" ")}
-                >
-                    <div
-                        className={[alreadyRendered ? undefined : animations.enter, props.className].join(" ")}
-                        style={props.styles}
-                        ref={target}
-                        onAnimationEnd={e => {
-                            if (e.target.classList.contains(animations.exit)) {
-                                setAlreadyRendered(false)
-                                props.handleClose()
-                                rooted.current = false
-                                localRoot.current.unmount()
-                            }
-                            else
-                                setAlreadyRendered(true)
-                        }}>
-                        {props.children}
-                    </div>
-                </div>
-            ))
-
-        } else if (alreadyRendered)
-            animate()
-    }, [props.children, props.open, alreadyRendered])
-
-
-    useEffect(() => {
-        document.addEventListener("mousedown", handleMouseDown)
-        return () => document.removeEventListener("mousedown", handleMouseDown)
-    }, [alreadyRendered, props.open, props.children])
-
-    return <div ref={source} style={{display: "none"}}/>
+   return ReactDOM.createPortal(isClosed && !props.open ? null : (
+      <div
+         style={{backdropFilter: `blur(${props.blurIntensity ? props.blurIntensity : "5px"})`}}
+         className={[styles.wrapper, props.variant === "fit" ? styles.fitContent : styles.fitPage, props.wrapperClassName, context.dark ? context.styles.dark : context.styles.light].join(" ")}
+      >
+         <div
+            className={[props.className, props.open ? animations.enter : animations.exit].join(" ")}
+            style={props.styles}
+            ref={target}
+            onAnimationEnd={() => {
+               console.log("ENDING")
+               if (!props.open) {
+                  props.handleClose()
+                  setIsClosed(true)
+               }
+               else
+                  setIsClosed(false)
+            }}>
+            {props.children}
+         </div>
+      </div>
+   ), document.body)
 }
 
 Modal.propTypes = {
 
-    wrapperClassName: PropTypes.string,
-    variant: PropTypes.oneOf(["fill", "fit"]),
-    animationStyle: PropTypes.oneOf(["slide-left", "slide-right", "fade", "slide-bottom", "none"]),
-    className: PropTypes.string,
-    blurIntensity: PropTypes.any,
-    styles: PropTypes.object,
-    open: PropTypes.bool.isRequired,
-    handleClose: PropTypes.func.isRequired,
-    children: PropTypes.node
+   wrapperClassName: PropTypes.string,
+   animationStyle: PropTypes.oneOf(["slide-left", "slide-right", "fade", "slide-bottom", "none"]),
+   className: PropTypes.string,
+   blurIntensity: PropTypes.any,
+   styles: PropTypes.object,
+   open: PropTypes.bool.isRequired,
+   handleClose: PropTypes.func.isRequired,
+   children: PropTypes.node
 }
